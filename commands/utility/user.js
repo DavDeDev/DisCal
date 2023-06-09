e58d4f4d5a4a388b354c417fd130e1b2367d9697
DavDeDev <pietrocoladavid@gmail.com>
2023-06-09 15:05:50 -0400

    feat: defer reply theory

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
 	category: 'utility',
@@ -6,6 +7,8 @@ module.exports = {
 		.setName('user')
 		.setDescription('Provides information about the user.'),
 	async execute(interaction) {
+		await interaction.deferReply(); // NOTE: for a better UX, Discord gives three seconds to return a reply, otherwise it will show an error message. If your command takes longer you can let them know by deferring the reply.
+		await wait(4000);
 		// interaction.user is the object representing the User who ran the command
 		// interaction.member is the GuildMember object, which represents the user in the specific guild
 		await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
