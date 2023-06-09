b64494836d9b6774a151a680214e8fdb5cbdd1bf
DavDeDev <pietrocoladavid@gmail.com>
2023-06-09 12:14:07 -0400

    fix: indentation

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ for (const folder of commandFolders) {
 		const command = require(filePath);
 		if ('data' in command && 'execute' in command) {
 			client.commands.set(command.data.name, command);
-		} else {
+		}
+		else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
 	}
