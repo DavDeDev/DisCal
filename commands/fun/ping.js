e5c677b8a89ca090564d02541060262a32c71a27
DavDeDev <pietrocoladavid@gmail.com>
2023-06-09 15:01:12 -0400

    feat: edit reply

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -1,7 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
-	cooldown: 5, // Optional: Set the cooldown in seconds for this command
+	cooldown: 10, // Optional: Set the cooldown in seconds for this command
 	category: 'fun', // Optional: Put the command in a category (NOTE: the folder name will be the category name)
 	data: new SlashCommandBuilder()
 		.setName('ping')
@@ -9,5 +10,7 @@ module.exports = {
 	async execute(interaction) {
 		// ephemeral : true means that only the user who used the command will see the reply
 		await interaction.reply({ content: 'Secret Pong!', ephemeral: true }); 
+		await wait(2000);
+		await interaction.editReply('Pong again!'); // We have 15 minutes to edit the reply
 	},
 };
