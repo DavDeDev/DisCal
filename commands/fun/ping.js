847c76fc9b105a35f55150011118c1ea40b71df7
DavDeDev <pietrocoladavid@gmail.com>
2023-06-09 14:15:13 -0400

    feat: add category to commands

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	cooldown: 5, // Optional: Set the cooldown in seconds for this command
+	category: 'fun', // Optional: Put the command in a category (NOTE: the folder name will be the category name)
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
