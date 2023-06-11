6e696fdc01fa139c16b922793f60622d96cda93b
DavDeDev <pietrocoladavid@gmail.com>
2023-06-11 17:43:33 -0400

    feat: OAuth2 try

---
+++ OAuth2/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { port } = require('./config.json');
+
+const app = express();
+
+app.get('/', (request, response) => {
+	return response.sendFile('index.html', { root: '.' });
+});
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
--- index.js
+++ index.js
@@ -1,51 +1,55 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-require('dotenv').config();
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // Organize cooldowns into a collection
-client.cooldowns = new Collection(); 
+client.cooldowns = new Collection();
 // <key: command name, value: Collection<key: UserID, value: lastUsageTimeStamp>>
 client.commands = new Collection();
 
 // Organize command files into subfolders and load them
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		}
-		else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
 }
 
 // Organize event files into subfolders and load them
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		console.log(`[EVENT] Loaded ONCE event ${event.name}`)
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		console.log(`[EVENT] Loaded event ${event.name}`)
-		client.on(event.name, (...args) => event.execute(...args));
-	}
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    console.log(`[EVENT] Loaded ONCE event ${event.name}`);
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    console.log(`[EVENT] Loaded event ${event.name}`);
+    client.on(event.name, (...args) => event.execute(...args));
+  }
 }
-
 
 // Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
