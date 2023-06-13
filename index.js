5d95871a0e1ec5f8e3dfb30ea42b4d912a6f684f
DavDeDev <pietrocoladavid@gmail.com>
2023-06-12 22:32:15 -0400

    fix: trying to fix login google

--- index.js
+++ index.js
@@ -1,24 +1,73 @@
 const fs = require('node:fs');
 const path = require('node:path');
+const process = require('process');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
+const {authenticate} = require('@google-cloud/local-auth');
 const { google } = require('googleapis');
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/calendar'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
-const GCP_TOKEN = JSON.parse(process.env.GCP_TOKEN);
-
-const auth = async () => {
-  console.log('auth');
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
   try {
-    return google.auth.fromJSON(GCP_TOKEN);
+    // console.log(TOKEN_PATH)
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
   } catch (err) {
-    console.log(err);
+    return null;
   }
-};
+}
 
-const calendar = google.calendar({ version: 'v3', auth });
+/**
+ * Serializes credentials to a file compatible with GoogleAUth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  console.log('Authorizing...');
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -68,8 +117,5 @@ for (const file of eventFiles) {
   }
 }
 
-// Log in to Discord with your client's token
-auth();
 
-client.login(process.env.DISCORD_TOKEN);
-console.log(auth())
+authorize().then(console.log("auth")).then(client.login(process.env.DISCORD_TOKEN));
