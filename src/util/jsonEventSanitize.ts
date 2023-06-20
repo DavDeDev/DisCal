deb786d91708cac963c6681762fedfa5ffa616a9
DavDeDev <pietrocoladavid@gmail.com>
2023-06-20 15:55:38 -0400

    fix: spacing

--- src/util/jsonEventSanitize.ts
+++ src/util/jsonEventSanitize.ts
@@ -1,38 +1,3 @@
-
-// {
-//     "kind": "calendar#event",
-//     "etag": "\"3370314625302000\"",
-//     "id": "_60q30c1g60o30e1i60o4ac1g60rj8gpl88rj2c1h84s34h9g60s30c1g60o30c1g8kojeca58orj4d1p74o48e9g64o30c1g60o30c1g60o30c1g60o32c1g60o30c1g84pjaci468s3edhg710k8c1k88sjacpk8gsj2h9h6p24ag9p8h10",
-//     "status": "confirmed",
-//     "htmlLink": "https://www.google.com/calendar/event?eid=XzYwcTMwYzFnNjBvMzBlMWk2MG80YWMxZzYwcmo4Z3BsODhyajJjMWg4NHMzNGg5ZzYwczMwYzFnNjBvMzBjMWc4a29qZWNhNThvcmo0ZDFwNzRvNDhlOWc2NG8zMGMxZzYwbzMwYzFnNjBvMzBjMWc2MG8zMmMxZzYwbzMwYzFnODRwamFjaTQ2OHMzZWRoZzcxMGs4YzFrODhzamFjcGs4Z3NqMmg5aDZwMjRhZzlwOGgxMCBmNjA5YzUzYmU3ODVhYmM2ZTgzMGE1OGZkNDA2NTlhOTY3M2RhNGRkYjlmZGU3NjY1ZTBhY2JmYmE2ODQzMGYyQGc",
-//     "created": "2023-05-27T03:15:12.000Z",
-//     "updated": "2023-05-27T03:15:12.651Z",
-//     "summary": "EthGlobal Waterlooo",
-//     "location": " 64 University Ave W, Waterloo, ON N2L 3C7, Canada",
-//     "creator": {
-//       "email": "pietrocoladavid@gmail.com"
-//     },
-//     "organizer": {
-//       "email": "f609c53be785abc6e830a58fd40659a9673da4ddb9fde7665e0acbfba68430f2@group.calendar.google.com",
-//       "displayName": "Events",
-//       "self": true
-//     },
-//     "start": {
-//       "dateTime": "2023-06-23T14:00:00-04:00",
-//       "timeZone": "America/Toronto"
-//     },
-//     "end": {
-//       "dateTime": "2023-06-25T20:00:00-04:00",
-//       "timeZone": "America/Toronto"
-//     },
-//     "iCalUID": "040000008200E00074C5B7101A82E00800000000E171EF724990D901000000000000000010000000A352D287608AD04B9534D91E16DEA9DB",
-//     "sequence": 0,
-//     "reminders": {
-//       "useDefault": true
-//     },
-//     "eventType": "default"
-//   },\
-
 import { EventType, ICalEvent } from 'types';
 
 export function jsonEventSanitize(json: any): ICalEvent {
@@ -47,6 +12,4 @@ export function jsonEventSanitize(json: any): ICalEvent {
     };
 
     return result;
-
-
 }
