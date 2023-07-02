6be73c007999423ba9b94090dd26d09fe5550742
DavDeDev <pietrocoladavid@gmail.com>
2023-07-02 17:37:17 -0400

    fix: dates displayed in embed message

--- src/classes/DescriptionMessage.ts
+++ src/classes/DescriptionMessage.ts
@@ -1,3 +1,4 @@
+import { time } from 'discord.js';
 import { EventType, IDescriptionEmbed } from 'types';
 
 export class DescriptionMessage implements IDescriptionEmbed {
@@ -6,15 +7,19 @@ export class DescriptionMessage implements IDescriptionEmbed {
     location: string;
     start: Date;
     end: Date;
-    constructor(type: EventType, cost: boolean, location: string, start: Date, end: Date) {
+    constructor(type: EventType, paid: boolean, location: string, start: Date, end: Date) {
         this.type = type;
-        this.cost = cost ? 'Free' : '$$';
+        this.cost = paid ? 'Free' : '$$';
         this.location = location;
         this.start = start;
         this.end = end;
     }
     toString(): string {
-        return `Type: ${this.type}\nCost: ${this.cost}\nLocation: ${this.location}\nStart: ${this.start}\nEnd: ${this.end}`;
+        console.log(this.start.getTime());
+        return `**${this.type} - ${this.cost}**
+        _${this.location}_
+        \n**TIME**
+        ${time(this.start)} - ${time(this.end)}`;
     }
 
 }
