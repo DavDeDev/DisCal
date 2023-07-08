b3571513a1ba22ba9aa73407a0d7c654620888fc
DavDeDev <pietrocoladavid@gmail.com>
2023-07-07 23:48:18 -0400

    fix: removed emoji from message title

--- src/classes/EmbedMessage.ts
+++ src/classes/EmbedMessage.ts
@@ -1,5 +1,5 @@
 import { EventType, ICalEvent } from 'types';
-import { APIEmbed, APIEmbedAuthor, APIEmbedField, APIEmbedImage, inlineCode } from 'discord.js';
+import { APIEmbed, APIEmbedAuthor, APIEmbedField, APIEmbedImage, channelLink, inlineCode } from 'discord.js';
 import { DescriptionMessage } from './DescriptionMessage';
 
 
@@ -7,7 +7,9 @@ import { DescriptionMessage } from './DescriptionMessage';
  * Represent the embed object with Calendar details
  */
 export class EmbedMessage implements APIEmbed {
-    // ? Author is always the same or URL of event
+    /**
+     * Author is used to display the link of the calendar or to advertise my GitHub XD
+     */
     author: APIEmbedAuthor = {
         name: 'Calendar',
         url: process.env.SHAREABLE_LINK_CALENDAR ?? 'https://github.com/DavDeDev',
@@ -19,7 +21,7 @@ export class EmbedMessage implements APIEmbed {
     image: APIEmbedImage;
 
     constructor(CalEvent: ICalEvent, image: string = EventType.getDefaultImage(CalEvent.type)) {
-        this.title = `${EventType.getEmoji(CalEvent.type)} - ${CalEvent.title}`;
+        this.title = `${CalEvent.title}`;
         this.url = CalEvent.url;
         // TODO: Add Width and Height
         this.image = { url: image };
