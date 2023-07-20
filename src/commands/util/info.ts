976899e70263111229f09845cf8212a9d056a6e0
DavDeDev <pietrocoladavid@gmail.com>
2023-07-20 10:38:34 -0400

    feat: info command

--- src/commands/util/info.ts
+++ src/commands/util/info.ts
@@ -1,5 +1,8 @@
 // import { ICommand } from '../../interfaces/ICommand';
 
+import { CacheType, ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { Command } from '../../classes';
+
 // class MyCommand implements ICommand {
 //   category: string;
 //   data: SlashCommandBuilder;
@@ -13,3 +16,16 @@
 //     // Add your implementation for the execute method here
 //   }
 // }
+
+export const info: Command = new Command(
+    __dirname,
+    new SlashCommandBuilder()
+        .setName('info')
+        .setDescription('Get information about the bot.'),
+    async (interaction: ChatInputCommandInteraction<CacheType>) => {
+        await interaction.reply({
+            content: 'This is a bot made for the [Hackathon Hackers](https://hackathonhackers.com) Discord server.',
+            ephemeral: true,
+        });
+    },
+);
